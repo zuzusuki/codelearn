d6e2fccf071c812c50b0d6fe586479333f17b929
minh1612 <khunglongtoiday123@gmail.com>
2019-07-17 08:36:14 +0700

    lading router

--- codelearn/resources/js/app.js
+++ codelearn/resources/js/app.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Landing from './components/header.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI)
 
 const app = new Vue({
     el: '#app',
